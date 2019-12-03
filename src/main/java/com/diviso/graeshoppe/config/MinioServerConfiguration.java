12fc008eedfbf8b9106ec1488b9496ac053afe6f
neerajamurali094 <neeraja.m@lxisoft.com>
2019-12-03 09:55:05 +0530

    initial commit

---
+++ src/main/java/com/diviso/graeshoppe/config/MinioServerConfiguration.java
@@ -0,0 +1,56 @@
+package com.diviso.graeshoppe.config;
+
+import org.jfree.util.Log;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.minio.MinioClient;
+import io.minio.errors.InvalidEndpointException;
+import io.minio.errors.InvalidPortException;
+
+@Configuration
+public class MinioServerConfiguration {
+
+	@Value("${minio.server.url}")
+	private String url;
+
+	@Value("${minio.server.accessKey}")
+	private String accesskey;
+
+	@Value("${minio.server.secretKey}")
+	private String secretKey;
+
+	@Value("${minio.buckets.product}")
+	private String productBucketName;
+
+	@Value("${minio.buckets.category}")
+	private String categoryBucketName;
+
+	@Bean
+	public MinioClient getMinioClient() throws InvalidEndpointException, InvalidPortException {
+		MinioClient minioClient = new MinioClient(url, accesskey, secretKey);
+		try {
+			boolean productBucketFound = minioClient.bucketExists(productBucketName);
+			boolean categoryBucketFound = minioClient.bucketExists(categoryBucketName);
+			if (productBucketFound) {
+				Log.info("ProductBucket already exists " + productBucketName);
+			} else {
+				minioClient.makeBucket(productBucketName);
+				Log.info("ProductBucket created " + productBucketName);
+			}
+
+			if (categoryBucketFound) {
+				Log.info("CategoryBucket already exists " + categoryBucketName);
+			} else {
+				minioClient.makeBucket(categoryBucketName);
+				Log.info("CategoryBucket created " + categoryBucketName);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return minioClient;
+	}
+
+}
---
+++ src/main/java/com/diviso/graeshoppe/service/ImageService.java
@@ -0,0 +1,64 @@
+package com.diviso.graeshoppe.service;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+//import com.diviso.graeshoppe.product.service.impl.ProductServiceImpl;
+
+import io.minio.MinioClient;
+
+@Service
+public class ImageService {
+
+	private final Logger log = LoggerFactory.getLogger(ImageService.class);
+	
+	@Autowired
+	private MinioClient minioClient;
+
+	@Value("${minio.buckets.product}")
+	private String productBucketName;
+
+	@Value("${minio.buckets.category}")
+	private String categoryBucketName;
+
+	@Value("${minio.configuration.contentType}")
+	private String contentType;
+	
+	@Value("${minio.server.url}")
+	private String url;
+
+
+	@SuppressWarnings("deprecation")
+	public String saveFile(String type, String entityId, byte[] data) {
+		InputStream streamData = new ByteArrayInputStream(data);
+		String bucket = null;
+		String imageName = null; 
+		String imageLink = url;
+		if (type.equals("product")) {
+			bucket = productBucketName;
+			imageName = entityId+"-product-image.png";
+			imageLink = url+"/"+bucket+"/"+imageName;
+			log.info("Saving the product Image in bucket "+bucket+" imagename is "+imageName+" image link is "+imageLink);
+		} else if (type.equals("category")) {
+			bucket = categoryBucketName;
+			imageName = entityId+"-category-image.png";
+			imageLink = url+"/"+bucket+"/"+imageName;
+			log.info("Saving the category Image in bucket "+bucket+" imagename is "+imageName+"  image link is "+imageLink);
+		}
+		try {
+			minioClient.putObject(bucket, imageName, streamData, contentType);
+			log.info("Image has been saved successfully");
+
+		} catch (Exception e) {
+			log.error("Something went wrong while saving image to miniob server "+ e.getMessage());
+		}
+		
+		return imageLink;
+	}
+
+
+}
